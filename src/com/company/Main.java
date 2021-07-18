c90e9335badade2a58beba494718b5eec91956dc
GoutamrajSahu <sahugoutamraj@gmail.com>
2021-07-18 23:04:06 +0530

    Add one more way to do it.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -39,3 +39,45 @@ class Main {
         }
     }
 }
+
+
+//----------------------------------OR--------------------------------------//
+
+
+//class Main {
+//    public static void main(String[] args){
+//        Scanner Sc = new Scanner(System.in);
+//        System.out.println("Enter the number of rows: ");
+//        int num = Sc.nextInt();
+//        int spaces = num-1;
+//        int chs = 1;
+//        int printValue = 1;
+//        int printValue2 = 0;
+//        int mid = (num+1)/2;
+//        for(int i = 0; i<num; i++){
+//            for(int j=1; j<spaces; j++){
+//                System.out.print(" ");
+//            }
+//            printValue2 = printValue;
+//            for(int k=0; k<chs; k++){
+//                System.out.print(printValue2);
+//                if(k<chs/2){
+//                    printValue2--;
+//                }else{
+//                    printValue2++;
+//                }
+//            }
+//            System.out.print("\n");
+//
+//            if(i>(mid)-2){
+//                printValue--;
+//                spaces = spaces + 1;
+//                chs = chs - 2;
+//            }else{
+//                printValue++;
+//                spaces = spaces-1;
+//                chs = chs+2;
+//            }
+//        }
+//    }
+//}
